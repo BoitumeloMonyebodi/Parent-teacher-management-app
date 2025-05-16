17c999627541e2dea1843d00878aa1b4ddc381f2
Boitumelo Monyebodi <linkymonyebodi1@gmail.com>
2025-05-16 19:27:16 +0200

    Create Models.py

---
+++ Backend/accounts/Models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email_verified = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profile_pictures/', null=True, blank=True)
+
+    def __str__(self):
+        return self.email
+
+    # delete profile picture image if user is deleted
+    def delete(self, using=None, keep_parents=False):
+        storage = self.profile_picture.storage
+
+        if self.profile_picture and storage.exists(self.profile_picture.name):
+            storage.delete(self.profile_picture.name)
+
+        super().delete()
